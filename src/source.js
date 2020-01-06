5ba8c02762435688fadd833e23b435c9c84027e5
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2020-01-06 16:58:11 +0100

    mark

--- src/source.js
+++ src/source.js
@@ -1,28 +1,132 @@
-import './source.scss';
-import 'ol/ol.css';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import {defaults as defaultInteractions, DragRotateAndZoom} from 'ol/interaction';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import {transform} from 'ol/proj';
-
+import "./source.scss";
+import "ol/ol.css";
+import Map from "ol/Map";
+import GeoJSON from "ol/format/GeoJSON";
+import View from "ol/View";
+import {
+  defaults as defaultInteractions,
+  DragRotateAndZoom
+} from "ol/interaction";
+import TileLayer from "ol/layer/Tile";
+import { OSM, Vector as VectorSource } from "ol/source";
+import { Circle as CircleStyle, Stroke, Style, Fill, Text } from "ol/style";
+import { transform } from "ol/proj";
+import VectorLayer from "ol/layer/Vector";
+ 
 // coordonnées récupérées depuis https://www.latlong.net/convert-address-to-lat-long.html
-var nws = transform([1.066530, 49.428470], 'EPSG:4326', 'EPSG:3857');
-
+const nws = transform([1.06653, 49.42847], "EPSG:4326", "EPSG:3857");
+const copeaux = transform([1.06539, 49.42251], "EPSG:4326", "EPSG:3857");
+ 
+const geojsonObject = {
+  type: "FeatureCollection",
+  crs: {
+    type: "name",
+    properties: {
+      name: "EPSG:3857"
+    }
+  },
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        name: "Normandie Web School"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: nws
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Les Copeaux"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: copeaux
+      }
+    }
+  ]
+};
+ 
+const pointStyle = new Style({
+  image: new CircleStyle({
+    radius: 5,
+    fill: new Fill({ color: "#3363FF" }),
+    stroke: new Stroke({ color: "#86A3FF", width: 2 })
+  })
+});
+ 
+const pointHoverStyle = new Style({
+  image: new CircleStyle({
+    radius: 5,
+    fill: new Fill({ color: "#3363FF" }),
+    stroke: new Stroke({ color: "#86A3FF", width: 2 })
+  }),
+  text: new Text({
+    textAlign: "center",
+    placement: "POINT",
+    font: "bold 22",
+    stroke: new Stroke({ color: "#3363FF", width: 1.5 }),
+    fill: new Fill({ color: "white" }),
+    scale: 1.8,
+    textBaseline: 'top',
+  })
+});
+ 
+const vectorPoints = new VectorLayer({
+  source: new VectorSource({
+    features: new GeoJSON().readFeatures(geojsonObject)
+  }),
+  style: pointStyle
+});
+ 
 var map = new Map({
-  interactions: defaultInteractions().extend([
-    new DragRotateAndZoom()
-  ]),
+  interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
   layers: [
     new TileLayer({
       source: new OSM()
-    })
+    }),
+    vectorPoints
   ],
-  target: 'carteNWS',
+  target: "carteNWS",
   view: new View({
-    projection: 'EPSG:3857',
+    projection: "EPSG:3857",
     center: nws,
     zoom: 14
   })
+});
+ 
+const featureOverlay = new VectorLayer({
+  source: new VectorSource(),
+  map: map,
+  style: feature => {
+    pointHoverStyle.getText().setText(feature.get("name"));
+    return pointHoverStyle;
+  }
+});
+ 
+var highlight;
+const displayFeatureInfo = pixel => {
+  vectorPoints.getFeatures(pixel).then(features => {
+    const feature = features.length ? features[0] : undefined;
+ 
+    if (feature !== highlight) {
+      if (highlight) {
+        featureOverlay.getSource().removeFeature(highlight);
+      }
+      if (feature) {
+        featureOverlay.getSource().addFeature(feature);
+      }
+      highlight = feature;
+    }
+  });
+};
+ 
+map.on("pointermove", e => {
+  if (e.dragging) {
+    return;
+  }
+  const pixel = map.getEventPixel(e.originalEvent);
+  displayFeatureInfo(pixel);
 });
